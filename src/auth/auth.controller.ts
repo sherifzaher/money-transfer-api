90bea2ee0ae079a7a4816745008df1ddebcdbc57
sherifzaher <sherifashrafmedia@gmail.com>
2023-05-20 00:48:58 +0300

    init auth endpoint

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { ROUTES } from '../utils/constants';
+
+@Controller(ROUTES.AUTH)
+export class AuthController {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 9001;
+  app.setGlobalPrefix('api');
+  app.enableCors({ origin: ['http://localhost:3000'], credentials: true });
+  app.useGlobalPipes(new ValidationPipe());
   try {
     await app.listen(PORT, () => console.log(`Running ChuaPay API on ${PORT}`));
   } catch (err) {
